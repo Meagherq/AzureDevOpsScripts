660169b8e5ae0d8ec562e43161d1fcea82065cb3
Quinn Meagher <quinnmeagher@microsoft.com>
2024-01-12 15:57:38 -0500

    Updated to include partition size and throughput

--- AzureCosmosDBThroughput.ps1
+++ AzureCosmosDBThroughput.ps1
@@ -1,12 +1,12 @@
-﻿$StartTime = "2023-12-01T00:00:00Z"
-$EndTime = "2024-01-07T00:00:00Z"
+﻿$StartTime = "2023-12-12T00:00:00Z"
+$EndTime = "2024-01-12T00:00:00Z"
 $TenantId = ""
 $ExcludeSouthCentralRegion = $false
 
 $Subscriptions = Get-AzSubscription -TenantId $TenantId
 
 $TotalRequestUnits = 0
-$NumberOfDays
+$NumberOfDays = $null
 $RequestUnitDictionary = @{}
 
 $subscriptions | ForEach-Object {
@@ -18,7 +18,7 @@ $subscriptions | ForEach-Object {
     $ResourceGroupsInSubscription | ForEach-Object {
 
         $CosmosDBAccounts = Get-AzCosmosDBAccount -ResourceGroupName $_.ResourceGroupName
-
+        $ResourceGroupName = $_.ResourceGroupName
         $CosmosDBAccounts | ForEach-Object {
             
             if ($ExcludeSouthCentralRegion -eq $true -and $_.Location -eq "South Central US") {
@@ -26,11 +26,51 @@ $subscriptions | ForEach-Object {
                 return
             }
 
+            $TotalCosmosDBProvisionedThroughtput = 0
+            #$TotalNumberOfPhysicalPartitions = 0
+            $IsServerless = $false
+            $_.Capabilities | ForEach-Object {
+                if ($_.Name -eq "EnableServerless") {
+                    $TotalCosmosDBProvisionedThroughtput = "Serverless"
+                    $IsServerless = $true
+                }
+            }
+            $AccountName = $_.Name
+            if ($IsServerless -eq $false) {
+                $Databases = Get-AzCosmosDBSqlDatabase  -ResourceGroupName $ResourceGroupName -AccountName $_.Name
+                
+                $Databases | ForEach-Object {
+                    
+                    $DatabaseThroughput = Get-AzCosmosDBSqlDatabaseThroughput -ResourceGroupName $ResourceGroupName -AccountName $AccountName -Name $_.Name
+
+                    if ($DatabaseThroughput.Throughput -ne $null) {
+                        $TotalCosmosDBProvisionedThroughtput += $DatabaseThroughput.Throughput
+                        #$TotalNumberOfPhysicalPartitions++
+                    }
+                    else {
+                        $Containers = Get-AzCosmosDBSqlContainer -ResourceGroupName $ResourceGroupName -AccountName $AccountName -DatabaseName $_.Name
+
+                        $DatabaseName = $_.Name
+        
+                        $Containers | ForEach-Object {
+
+                            $ContainerThroughput = Get-AzCosmosDBSqlContainerThroughput -ResourceGroupName $ResourceGroupName -AccountName $AccountName -DatabaseName $DatabaseName -Name $_.Name
+                            if ($ContainerThroughput.Throughput -ne $null) {
+                                $TotalCosmosDBProvisionedThroughtput += $ContainerThroughput.Throughput
+                                #$TotalNumberOfPhysicalPartitions++
+                            }
+                        }
+                    }
+                    
+                }
+            }
+
             $IndividualCosmosAccountRequestUnits = 0
             $PeakDailyRUConsumption = 0
             $PeakDailyRUDate = "No Date"
 
-            $MetricData = Get-AzMetric -ResourceId $_.Id -MetricName TotalRequestUnits -TimeGrain 01:00:00:00 -StartTime $StartTime -EndTime $EndTime -AggregationType Total -WarningAction SilentlyContinue
+            $MetricData = Get-AzMetric -ResourceId $_.Id -MetricName TotalRequestUnits -TimeGrain 01:00:00:00 -StartTime $StartTime -EndTime $EndTime -AggregationType Total -WarningAction SilentlyContinue   
+
             $NumberOfDays = $MetricData.Data.Count
             $MetricData.Data | ForEach-Object {
                 $IndividualCosmosAccountRequestUnits += $_.Total
@@ -42,15 +82,26 @@ $subscriptions | ForEach-Object {
                 }
             }
 
+            $PhysicalParititionSize = Get-AzMetric -ResourceId $_.Id -MetricName PhysicalPartitionSizeInfo -TimeGrain 01:00:00:00 -StartTime $StartTime -EndTime $EndTime -AggregationType Max -WarningAction SilentlyContinue
+            $TotalPhysicalPartitionSize = 0
+            if ($PhysicalParititionSize.Data -ne $null) {
+                $TotalPhysicalPartitionSize = $PhysicalParititionSize.Data[$PhysicalParititionSize.Data.Count - 1].Maximum
+            }
+
+            $FormattedPhysicalParitionSize = [math]::round($TotalPhysicalPartitionSize / 1Gb, 5)
+
             $IndividualCosmosAccountContext = @{
-                TotalRequestUnits = $IndividualCosmosAccountRequestUnits
+                TotalRequestUnits                   = $IndividualCosmosAccountRequestUnits
                 # Total Request Units Per Seconds is the total request units consumed divided by the number of days, hours, minutes, and seconds in the time range.
-                TotalRequestUnitsPerSecond = $IndividualCosmosAccountRequestUnits / $MetricData.Data.Count / 24 / 60 / 60 
-                Region = $_.Location
-                PeakDailyRUConsumption = $PeakDailyRUConsumption
+                TotalRequestUnitsPerSecond          = $IndividualCosmosAccountRequestUnits / $NumberOfDays / 24 / 60 / 60 
+                Region                              = $_.Location
+                PeakDailyRUConsumption              = $PeakDailyRUConsumption
                 # Peak Daily Request Units Per Seconds is the peak daily request units consumed divided by the number of hours, minutes, and seconds in a day.
-                PeakDailyRUPerSecondConsumption = $PeakDailyRUConsumption / 24 / 60 / 60
-                PeakDailyRUPerSecondDate = $PeakDailyRUDate
+                PeakDailyRUPerSecondConsumption     = $PeakDailyRUConsumption / 24 / 60 / 60
+                PeakDailyRUPerSecondDate            = $PeakDailyRUDate
+                TotalCosmosDBProvisionedThroughtput = $TotalCosmosDBProvisionedThroughtput
+                TotalPhysicalPartitionSize          = "$FormattedPhysicalParitionSize GB"
+                #TotalNumberOfPhysicalPartitions     = $TotalNumberOfPhysicalPartitions
             }
 
             $RequestUnitDictionary.Add($_.Name, $IndividualCosmosAccountContext)
